2afcd558622a39272b278987db5dbe5398025578
DoubleChuang <ethan9141@gmail.com>
2023-03-14 02:35:23 +0800

    fix: display the ccxt version when the program starts

--- ccxt_bot/__main__.py
+++ ccxt_bot/__main__.py
@@ -2,6 +2,7 @@
 
 import asyncio
 
+import ccxt
 from ccxt_bot.trade.bot import Ccxt_bot
 from ccxt_bot.core.logger import logger
 from ccxt_bot.trade.stragtegy import MjStategy
@@ -10,7 +11,8 @@ import ccxt_bot
 
 
 async def main():
-    logger.info(f"version: {ccxt_bot.__commit_hash__}")
+    logger.info(f"ccxt version: {ccxt.__version__}")
+    logger.info(f"ccxt bot commit hash: {ccxt_bot.__commit_hash__}")
     bot = Ccxt_bot(
         api_key=config.BINANCE_API_KEY,
         secret=config.BINANCE_SECRET,
